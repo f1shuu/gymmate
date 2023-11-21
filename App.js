96d004cbef083a9a3d5c6c90425cc5d736be2c92
f1shu <szulzyckifilip@gmail.com>
2023-11-21 19:16:40 +0100

    Add animated splash screen

--- App.js
+++ App.js
@@ -1,20 +1,54 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useEffect } from 'react';
+import { View, Text, Image } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
 
-export default function App() {
+function Loader({ navigation }) {
+  useEffect(() => {
+    const gifTimeout = setTimeout(() => {
+      navigation.navigate('HomeScreen');
+    }, 2500);
+
+    return () => clearTimeout(gifTimeout);
+  }, [navigation]);
+
   return (
-    <View style={styles.container}>
-      <Text>TEST</Text>
-      <StatusBar style="auto" />
+    <View style={{ flex: 1, backgroundColor: '#376DEC', alignItems: 'center', justifyContent: 'center' }}>
+      <Image
+        source={require('./assets/splash.gif')}
+        style={{ resizeMode: 'center' }}
+      />
     </View>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+function HomeScreen() {
+  return (
+    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+      <Text>Home Screen</Text>
+    </View>
+  );
+}
+
+const Stack = createNativeStackNavigator();
+
+function App() {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator initialRouteName="Loader">
+        <Stack.Screen
+          name="Loader"
+          component={Loader}
+          options={{ headerShown: false }}
+        />
+        <Stack.Screen
+          name="HomeScreen"
+          component={HomeScreen}
+          options={{ headerShown: false, animation: 'none' }}
+        />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+}
+
+export default App;
