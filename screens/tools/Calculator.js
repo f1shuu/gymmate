3ae1eff08ac7c44006cff44d25825441dab114c5
f1shu <szulzyckifilip@gmail.com>
2023-11-29 23:46:39 +0100

    [v0.5.2] Add calculator screen

--- screens/tools/Calculator.js
+++ screens/tools/Calculator.js
@@ -1,8 +1,123 @@
-import { View, StyleSheet } from 'react-native';
+import { View, Text, Image, StyleSheet, TouchableOpacity } from 'react-native';
+import { useState } from 'react';
 
 export default function Calculator() {
+    const [input, setInput] = useState('');
+    const [output, setOutput] = useState('');
+    const calculate = (value) => {
+        const replaceSymbols = (expr) => expr.replace(/x/g, '*').replace(/÷/g, '/').replace(/\^/g, '**');
+        if (value === '=') {
+            try {
+                const result = replaceSymbols(input);
+                let newOutput = eval(result).toPrecision(8).toString();
+                newOutput = parseFloat(newOutput);
+                setOutput(newOutput);
+            } catch (error) {
+                setOutput('Błąd');
+            }
+        } else if (value === 'C') {
+            setInput('');
+            setOutput('');
+        } else if (value === 'sqrt') {
+            try {
+                let newOutput = Math.sqrt(eval(replaceSymbols(input))).toPrecision(8).toString();
+                newOutput = parseFloat(newOutput);
+                setOutput(newOutput);
+            } catch (error) {
+                setOutput('Błąd');
+            }
+        } else if (value === 'backspace') {
+            setInput((prevInput) => prevInput.slice(0, -1));
+        } else {
+            const lastChar = input.slice(-1);
+            const isLastCharSymbol = ['+', '-', 'x', '÷', '^'].includes(lastChar);
+            if (isLastCharSymbol) {
+                if (['+', '-', 'x', '÷', '^'].includes(value)) setInput((prevInput) => prevInput.slice(0, -1) + value);
+                else setInput((prevInput) => prevInput + value);
+            } else setInput((prevInput) => prevInput + value);
+        }
+    };
+
     return (
-        <View style={styles.container} />
+        <View style={styles.container}>
+            <Text style={styles.input}>{input}</Text>
+            <Text style={styles.output}>{output}</Text>
+            <View style={styles.buttons}>
+                <View style={styles.row}>
+                    <TouchableOpacity style={styles.button} onPress={() => calculate('C')}>
+                        <Text style={styles.delete}>C</Text>
+                    </TouchableOpacity>
+                    <TouchableOpacity style={styles.button} onPress={() => calculate('sqrt')}>
+                        <Text style={styles.symbols}>√</Text>
+                    </TouchableOpacity>
+                    <TouchableOpacity style={styles.button} onPress={() => calculate('^')}>
+                        <Text style={styles.symbols}>^</Text>
+                    </TouchableOpacity>
+                    <TouchableOpacity style={styles.button} onPress={() => calculate('÷')}>
+                        <Text style={styles.symbols}>÷</Text>
+                    </TouchableOpacity>
+                </View>
+                <View style={styles.row}>
+                    <TouchableOpacity style={styles.button} onPress={() => calculate('7')}>
+                        <Text style={styles.numbers}>7</Text>
+                    </TouchableOpacity>
+                    <TouchableOpacity style={styles.button} onPress={() => calculate('8')}>
+                        <Text style={styles.numbers}>8</Text>
+                    </TouchableOpacity>
+                    <TouchableOpacity style={styles.button} onPress={() => calculate('9')}>
+                        <Text style={styles.numbers}>9</Text>
+                    </TouchableOpacity>
+                    <TouchableOpacity style={styles.button} onPress={() => calculate('x')}>
+                        <Text style={styles.symbols}>x</Text>
+                    </TouchableOpacity>
+                </View>
+                <View style={styles.row}>
+                    <TouchableOpacity style={styles.button} onPress={() => calculate('4')}>
+                        <Text style={styles.numbers}>4</Text>
+                    </TouchableOpacity>
+                    <TouchableOpacity style={styles.button} onPress={() => calculate('5')}>
+                        <Text style={styles.numbers}>5</Text>
+                    </TouchableOpacity>
+                    <TouchableOpacity style={styles.button} onPress={() => calculate('6')}>
+                        <Text style={styles.numbers}>6</Text>
+                    </TouchableOpacity>
+                    <TouchableOpacity style={styles.button} onPress={() => calculate('-')}>
+                        <Text style={styles.symbols}>-</Text>
+                    </TouchableOpacity>
+                </View>
+                <View style={styles.row}>
+                    <TouchableOpacity style={styles.button} onPress={() => calculate('1')}>
+                        <Text style={styles.numbers}>1</Text>
+                    </TouchableOpacity>
+                    <TouchableOpacity style={styles.button} onPress={() => calculate('2')}>
+                        <Text style={styles.numbers}>2</Text>
+                    </TouchableOpacity>
+                    <TouchableOpacity style={styles.button} onPress={() => calculate('3')}>
+                        <Text style={styles.numbers}>3</Text>
+                    </TouchableOpacity>
+                    <TouchableOpacity style={styles.button} onPress={() => calculate('+')}>
+                        <Text style={styles.symbols}>+</Text>
+                    </TouchableOpacity>
+                </View>
+                <View style={styles.row}>
+                    <TouchableOpacity style={styles.button} onPress={() => calculate('0')}>
+                        <Text style={styles.numbers}>0</Text>
+                    </TouchableOpacity>
+                    <TouchableOpacity style={styles.button} onPress={() => calculate('.')}>
+                        <Text style={styles.numbers}>.</Text>
+                    </TouchableOpacity>
+                    <TouchableOpacity style={styles.button} onPress={() => calculate('backspace')}>
+                        <Image
+                            source={require('../../assets/tools/calculator/backspace.png')}
+                            style={{ width: 40, height: 40 }}
+                        />
+                    </TouchableOpacity>
+                    <TouchableOpacity style={styles.button} onPress={() => calculate('=')}>
+                        <Text style={styles.symbols}>=</Text>
+                    </TouchableOpacity>
+                </View>
+            </View>
+        </View>
     );
 }
 
@@ -10,5 +125,49 @@ const styles = StyleSheet.create({
     container: {
         backgroundColor: '#141414',
         flex: 1
-    }
+    },
+    buttons: {
+        marginBottom: 5
+    },
+    row: {
+        flexDirection: 'row',
+        justifyContent: 'space-between',
+        marginTop: 10
+    },
+    input: {
+        color: 'white',
+        fontSize: 28,
+        textAlign: 'right',
+        paddingRight: 20,
+        paddingTop: 5
+    },
+    output: {
+        color: 'white',
+        fontSize: 44,
+        textAlign: 'right',
+        paddingTop: 10,
+        paddingRight: 20,
+        paddingBottom: 20
+    },
+    delete: {
+        color: '#FD5056',
+        fontSize: 36
+    },
+    symbols: {
+        color: '#5AFF98',
+        fontSize: 36
+    },
+    numbers: {
+        color: 'white',
+        fontSize: 36
+    },
+    button: {
+        flex: 1,
+        backgroundColor: '#2B2B2B',
+        justifyContent: 'center',
+        alignItems: 'center',
+        marginHorizontal: 5,
+        height: 85,
+        borderRadius: 50,
+    },
 })
