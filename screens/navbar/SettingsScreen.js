129909ba8760baeddc5f21cee0cb21772e1ed8e5
f1shuu <szulzyckifilip@gmail.com>
2025-01-31 00:22:01 +0100

    [v0.9.3] Add remaining settings + replace navbar PNGs with lightweight icons

--- screens/navbar/SettingsScreen.js
+++ screens/navbar/SettingsScreen.js
@@ -1,8 +1,7 @@
-import { Text, View } from 'react-native';
+import { Text, View, ScrollView } from 'react-native';
 import { useNavigation } from '@react-navigation/native';
 import Icon from 'react-native-vector-icons/MaterialIcons';
 
-import Background from '../../components/Background';
 import Colors from '../../Colors';
 import Container from '../../components/Container';
 import Setting from '../../components/widgets/Setting';
@@ -12,18 +11,40 @@ export default function SettingsScreen() {
 
     return (
         <Container gradient={true} gradientLength={0.5}>
-            <View style={styles.avatar}>
-                <Icon name={'account-circle'} size={100} color={Colors.white} style={styles.icon} />
-            </View>
-            <Text style={[styles.text, { fontSize: 28 }]}>Gość</Text>
-            <Text style={[styles.text, { marginBottom: 30 }]}>@gosc</Text>
-            <View style={styles.container}>
-                <Setting active={false} name='Ustawienia aplikacji' icon={'settings'} color={Colors.white} onPress='' />
-                <Setting active={false} name='Język' icon={'language'} color={Colors.white} onPress='' />
-                <Setting active={false} name='Personalizacja' icon={'tune'} color={Colors.white} nPress='' />
-                <Setting active={true} name='Usuwanie danych' icon={'delete'} color={Colors.white} onPress={() => navigation.navigate('DataDeletionScreen')} />
-                <Setting active={false} name='Informacje o aplikacji' icon={'info'} color={Colors.white} onPress='' />
-            </View>
+            <ScrollView>
+                <View style={styles.avatar}>
+                    <Icon name={'account-circle'} size={100} color={Colors.white} style={styles.icon} />
+                </View>
+                <Text style={styles.name}>Gość</Text>
+                <Text style={[styles.name, { fontSize: 16 }]}>@gosc</Text>
+                <View style={styles.container}>
+                    <Text style={styles.sectionName}>Ustawienia</Text>
+                    <Setting active={false} name='Powiadomienia' icon={'notifications'} color={Colors.white} onPress='' toggle={true} />
+                    <Setting active={false} name='Dźwięk' icon={'volume-up'} color={Colors.white} onPress='' toggle={true} />
+                    <Setting active={false} name='Wibracje' icon={'vibration'} color={Colors.white} onPress='' toggle={true} />
+                    <Setting active={false} name='Treningi' icon={'workspace-premium'} color={Colors.white} onPress='' />
+                    <Setting active={false} name='Eksport i migracja danych' icon={'loop'} color={Colors.white} onPress='' />
+                </View>
+                <View style={styles.container}>
+                    <Text style={styles.sectionName}>Personalizacja</Text>
+                    <Setting active={false} name='Motyw' icon={'palette'} color={Colors.white} onPress='' />
+                    <Setting active={false} name='Język' icon={'translate'} color={Colors.white} onPress='' />
+                    <Setting active={false} name='Jednostki' icon={'swap-horiz'} color={Colors.white} onPress='' />
+                </View>
+                <View style={styles.container}>
+                    <Text style={styles.sectionName}>Pomoc</Text>
+                    <Setting active={false} name='Informacje o aplikacji' icon={'info'} color={Colors.white} onPress='' />
+                    <Setting active={false} name='Oceń aplikację' icon={'star'} color={Colors.white} onPress='' />
+                    <Setting active={false} name='Polityka prywatności' icon={'text-snippet'} color={Colors.white} onPress='' />
+                    <Setting active={false} name='Zasady i warunki' icon={'menu-book'} color={Colors.white} onPress='' />
+                    <Setting active={false} name='Kontakt' icon={'mail'} color={Colors.white} onPress='' />
+                    <Setting active={false} name='Dziennik zmian' icon={'list-alt'} color={Colors.white} onPress='' />
+                </View>
+                <View style={styles.container}>
+                    <Text style={styles.sectionName}>Strefa ryzyka</Text>
+                    <Setting active={true} name='Usuwanie danych' icon={'delete'} color={Colors.red} onPress={() => navigation.navigate('DataDeletionScreen')} />
+                </View>
+            </ScrollView>
         </Container>
     )
 }
@@ -35,18 +56,23 @@ const styles = {
         alignItems: 'center',
         marginBottom: 10
     },
-    text: {
+    name: {
         fontFamily: 'Nexa',
-        fontSize: 16,
+        fontSize: 28,
         color: Colors.white,
         alignSelf: 'center',
         justifyContent: 'center',
         marginBottom: 10
     },
     container: {
-        gap: 10,
-        borderRadius: 15,
-        overflow: 'hidden',
+        gap: 5,
         marginTop: 20
+    },
+    sectionName: {
+        fontFamily: 'Nexa',
+        fontSize: 14,
+        color: Colors.secondary,
+        marginLeft: 10,
+        marginBottom: 10
     }
 }
