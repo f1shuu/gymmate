8313fced7f15147cc8a61781c12ba6cc1178829c
f1shuu <szulzyckifilip@gmail.com>
2026-08-05 22:40:22 +0200

    v1.1

--- app.config.js
+++ app.config.js
@@ -2,7 +2,7 @@ export default {
     expo: {
         name: 'GymMate',
         slug: 'GymMate',
-        version: '1.0.0',
+        version: '1.1.0',
         orientation: 'portrait',
         icon: './assets/images/icon.png',
         splash: {
