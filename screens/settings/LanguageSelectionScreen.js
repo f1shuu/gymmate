5dbd89846762a8e003616c4f1d57d631650e9a05
f1shuu <szulzyckifilip@gmail.com>
2026-08-14 19:27:40 +0200

    fix: hide unimplemented language select option

--- screens/settings/LanguageSelectionScreen.js
+++ screens/settings/LanguageSelectionScreen.js
@@ -19,7 +19,7 @@ export default function LanguageSelectionScreen() {
             <View style={{ gap: 5 }}>
                 <Setting active={true} name='🇵🇱 Polski' color={theme.textPrimary} onPress={() => changeLanguage('pl')} type={settings.language === 'pl' ? 'check' : null} style={{ paddingLeft: -10 }} />
                 <Setting active={true} name='🇬🇧 English' color={theme.textPrimary} onPress={() => changeLanguage('en')} type={settings.language === 'en' ? 'check' : null} style={{ paddingLeft: -10 }} />
-                <Setting active={true} name='🇪🇸 Español' color={theme.textPrimary} onPress={() => changeLanguage('es')} type={settings.language === 'es' ? 'check' : null} style={{ paddingLeft: -10 }} />
+                {/* <Setting active={true} name='🇪🇸 Español' color={theme.textPrimary} onPress={() => changeLanguage('es')} type={settings.language === 'es' ? 'check' : null} style={{ paddingLeft: -10 }} /> */}
             </View>
         </Container>
     )
